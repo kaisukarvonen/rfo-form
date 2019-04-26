2c19fd250045110528e242fd3680c32b4b5a3064
kaisukarvonen <karvonenkaisu@gmail.com>
2019-04-26 22:39:10 +0300

    remove unneeded files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import Root from './Root';
-
-ReactDOM.render(<Root />, document.getElementById('root'));
